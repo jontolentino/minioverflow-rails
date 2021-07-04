d553b736e89d68058b269b0ba02938e853ff5a39
Jon Tolentino <jontolentino123@gmail.com>
2021-07-04 22:13:40 +0800

    validatiions for question class

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,2 +1,9 @@
 class Question < ApplicationRecord
+    validates :title, presence: true
+    validates :content, presence: true
+    validates :name, presence: true
+    validates :subject, presence: true
+
+    # title,content,name,subject
+
 end
